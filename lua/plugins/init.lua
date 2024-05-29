c6c7fb2e0a4724f05a9f28dc468aa87380845ff5
Ninzalo <k-artemiy@mail.ru>
2024-05-29 14:16:57 +0300

    refactor: minor refactor

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,11 +1,4 @@
 return {
 	{ "folke/neoconf.nvim", cmd = "Neoconf" },
-	{
-		"folke/neodev.nvim",
-		-- config = function()
-		-- require("neodev").setup({
-		-- 	library = { plugins = { "neotest" }, types = true },
-		-- })
-		-- end,
-	},
+	{ "folke/neodev.nvim" },
 }
