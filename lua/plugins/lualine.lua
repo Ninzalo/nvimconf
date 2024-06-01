f82a8fad87d3010f082f6927f04d762d65d482f6
Ninzalo <k-artemiy@mail.ru>
2024-06-02 00:12:47 +0300

    feat(plugins): ui update

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,3 +1,19 @@
+local function truncate_branch_name(branch)
+	if not branch or branch == "" then
+		return ""
+	end
+
+	-- Match the branch name to the specified format
+	local user, team, ticket_number = string.match(branch, "^(%w+)/(%w+)%-(%d+)")
+
+	-- If the branch name matches the format, display {user}/{team}-{ticket_number}, otherwise display the full branch name
+	if ticket_number then
+		return user .. "/" .. team .. "-" .. ticket_number
+	else
+		return branch
+	end
+end
+
 local config = function()
 	require("lualine").setup({
 		options = {
@@ -8,8 +24,13 @@ local config = function()
 		},
 		sections = {
 			lualine_a = { "mode" },
-			lualine_b = { "buffers" },
-			lualine_x = { "encoding", "fileformat", "filetype" },
+			lualine_b = {
+				{ "branch", icon = "", fmt = truncate_branch_name },
+				"diagnostics",
+				"diff",
+			},
+			lualine_c = {},
+			lualine_x = { "filetype" },
 			lualine_y = { "progress" },
 			lualine_z = { "location" },
 		},
