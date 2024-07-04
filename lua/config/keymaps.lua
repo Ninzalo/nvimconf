0ea0fecbe78421ad62d314854b5c2553ef75bbc2
Ninzalo <k-artemiy@mail.ru>
2024-07-04 16:21:33 +0300

    chore: update dependencies

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,18 +3,18 @@ local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 -- Directory Navigation
-keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+keymap("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", opts)
 
 -- Pane Navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>", opts)
+keymap("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>", opts)
+keymap("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>", opts)
+keymap("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
-keymap("n", "gw", ":bd<CR>", opts)
+keymap("n", "<S-l>", "<cmd>bnext<CR>", opts)
+keymap("n", "<S-h>", "<cmd>bprevious<CR>", opts)
+keymap("n", "gw", "<cmd>bd<CR>", opts)
 
 -- Insert
 keymap("i", "jk", "<ESC>", opts)
